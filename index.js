1d77472bfb508f682d7b2b3e91536653c2b615b7
fan1ech00 <kravchuk.swe@gmail.com>
2022-11-27 12:30:17 +0200

    add mystat to google calendar sync

--- index.js
+++ index.js
@@ -1,27 +1,23 @@
-// import MystatAPI from "mystat-api";
+import MystatAPI from "mystat-api"
+import fs from "fs/promises"
+import path from 'path'
+import process from "process"
+import { authenticate } from "@google-cloud/local-auth"
+import { google, calendar_v3 } from "googleapis"
+import { RunBatch } from "gbatchrequests"
+import { DateTime } from "luxon";
 
-// const userData = {
-//   username: "Krav_up8d",
-//   password: "itSTEP1992",
-// };
-// const api = new MystatAPI(userData);
-
-// api.getMonthSchedule().then((result) => {
-//   if (result.success) {
-//     console.log(result.data);
-//   } else {
-//     console.log(result.error);
-//   }
-// });
-
-const fs = require('fs').promises;
-const path = require('path');
-const process = require('process');
-const { authenticate } = require('@google-cloud/local-auth');
-const {google, calendar_v3} = require('googleapis');
+const userData = {
+  username: "Krav_up8d",
+  password: "itSTEP1992",
+};
+const mystatApi = new MystatAPI(userData);
+if (typeof (await mystatApi.getAccessToken()) !== 'string') {
+  throw new Error("Invalid сredentials for mystat")
+}
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/calendar'];
+const SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/calendar.events'];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
@@ -107,15 +103,114 @@ async function listEvents(auth) {
 }
 
 async function createCalendar(auth) {
-  const calendar = google.calendar({version: 'v3', auth});
+  const calendar = google.calendar({version: 'v3', auth})
+  let calendarId = {}
 
-  let res = await calendar.calendars.insert({
-    requestBody: {
-      summary: 'test name',
-      timeZone: 'Europe/Kyiv',
+  try {
+    let res = await calendar.calendars.insert({
+      requestBody: {
+        summary: 'IT STEP',
+        timeZone: 'Europe/Kyiv',
+      }
+    })
+    calendarId = res.data.id
+    console.log(`Create new calendar with id: ${calendarId}`)
+  }
+  catch(err) {
+    throw err
+  }
+
+  await addMystatEvent(auth, calendarId)
+}
+
+async function addMystatEvent(auth, calendarId) {
+  // const calendar = google.calendar({version: 'v3', auth})
+
+  const dateStart = DateTime.local(2022, 1, 28)
+  const dateEnd = DateTime.local(2022, 12, 15)
+  if (dateStart > dateEnd)
+    throw new Error("dateStart > dateEnd")
+  if (!dateStart.isValid)
+    throw new Error("dateStart invalid")
+  if (!dateEnd.isValid)
+    throw new Error ("dateEnd invalid")
+
+  const months = getMonthArray(dateStart, dateEnd)
+
+  let mystatSchedule = []
+  for (let i = 0; i < months.length; i++) {
+    const month = months[i];
+
+    let response = {}
+    response = await mystatApi.getMonthSchedule(month.toJSDate())
+    if (!response.success) {
+      let attempts = 3
+      while (true) {
+        console.log(`Unsuccessful attempt to get data from mystat api. Attempts left: ${attempts}`)
+        --attempts
+        await delay(1000)
+        response = await mystatApi.getMonthSchedule(month.toJSDate())
+        if (response.success)
+          break
+        if (attempts == 0)
+          throw new Error("Mystat api is not responding")
+      }
+    }
+
+    if (i == 0) {
+      response.data = response.data.filter(value => DateTime.fromISO(value.date) >= dateStart)
+    }
+    if (i == months.length - 1) {
+      response.data = response.data.filter(value => DateTime.fromISO(value.date) <= dateEnd)
+    }
+    mystatSchedule.push(...response.data)
+  }
+
+  const calendarEvents = mystatSchedule.map((mystatEvent) => {
+    return {
+      summary: mystatEvent.subject_name,
+      location: mystatEvent.room_name,
+      description: `Teacher: ${mystatEvent.teacher_name}`,
+      start: {
+        dateTime: new Date(Date.parse(`${mystatEvent.date}:${mystatEvent.started_at}`)).toISOString(),
+        timeZone: 'Europe/Kyiv'
+      },
+      end: {
+        dateTime: new Date(Date.parse(`${mystatEvent.date}:${mystatEvent.finished_at}`)).toISOString(),
+        timeZone: 'Europe/Kyiv'
+      }
     }
   })
-  console.log(res.data)
+
+  const obj = {
+    accessToken: auth.credentials.access_token,
+    api: { name: "calendar", version: "v3" },
+    requests: calendarEvents.map((e) => ({
+      method: "POST",
+      endpoint: `https://www.googleapis.com/calendar/v3/calendars/${calendarId}/events`,
+      requestBody: e,
+    })),
+  };
+  RunBatch(obj)
+    .then((res) => console.log("Events uploaded to the google calendar"))
+    .catch((err) => console.error(err));
 }
+
+function getMonthArray (startDate, endDate) {
+  startDate = startDate.set({day: 1})
+  endDate = endDate.set({day: 1})
+  const months = []
+  let currentDate = startDate
+
+  while (currentDate <= endDate) {
+      months.push(new DateTime(currentDate))
+      currentDate = currentDate.plus({month: 1})
+  }
+  return months
+}
+
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+} 
 
 authorize().then(createCalendar).catch(console.error);
